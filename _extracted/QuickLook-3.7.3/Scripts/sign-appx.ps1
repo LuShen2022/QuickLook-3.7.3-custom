2efc9f155439ef38822b5a080980ad1c455268d3
牢🦌 <100514755+LuShen2022@users.noreply.github.com>
2026-09-14 16:21:12 +0800

    Delete _extracted/QuickLook-3.7.3 directory

--- _extracted/QuickLook-3.7.3/Scripts/sign-appx.ps1
+++
@@ -1,5 +0,0 @@
-﻿$signExe = 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\signtool.exe'
-$files = gci '..\Build\QuickLook-*.appx' | %{('"{0}"' -f $_.FullName)}
-$timestampUrl = 'http://time.certum.pl/'
-
-.$signExe sign /a /v /fd sha256 /t $timestampUrl $files
